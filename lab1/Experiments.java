8c6b30595a2c4dbfba6af47f6bed3a4553f41cf3
Arina <a.zvarunchyk@gmail.com>
2025-09-13 02:34:12 +0300

    corrected the cycle and time calculations

--- lab1/Experiments.java
+++ lab1/Experiments.java
@@ -7,51 +7,53 @@ public class Experiments{
         int maxVal=70000;
         int exper=1000;
 
+        double[] timeIn = new double[sizes.length];
+        double[] timeNotIn = new double[sizes.length];
+        double[] timeUnion = new double[sizes.length];
+
         Random rnd = new Random();
-        for (int size:sizes){
+        for (int i = 0; i < sizes.length; i++) {
+            int size = sizes[i];
             SparseSet set = generateRandomSet(maxVal, capacity, size);
-            int[] elInSet=new int[exper];
-            for(int i=0;i<exper;i++){
-                elInSet[i] = set.getEl(rnd.nextInt(set.size()));
+            int[] elInSet = new int[exper];
+            for (int j = 0; j < exper; j++) {
+                elInSet[j] = set.getEl(rnd.nextInt(set.size()));
             }
             long startTime = System.nanoTime();
-            for (int i=0;i<exper;i++){
-                set.search(elInSet[i]);
+            for (int j = 0; j < exper; j++) {
+                set.search(elInSet[j]);
             }
             long endTime = System.nanoTime();
-            long executionTime= endTime - startTime;
-            double totalTime = executionTime/(double)exper;
+            timeIn[i] = ((endTime - startTime) / (double) exper) / 1000.0;
 
-            int[] elNotInSet = new int [exper];
-            for(int i=0;i<exper;i++){
+            int[] elNotInSet = new int[exper];
+            for (int j = 0; j < exper; j++) {
                 int v;
                 do {
-                    v=rnd.nextInt(maxVal+1);
-                }
-                while (set.search(v)!=-1) ;
-                elNotInSet[i]=v;
+                    v = rnd.nextInt(maxVal + 1);
+                } while (set.search(v) != -1);
+                elNotInSet[j] = v;
             }
             long startTime1 = System.nanoTime();
-            for(int i=0;i<exper;i++){
-                set.search(elNotInSet[i]);
+            for (int j = 0; j < exper; j++) {
+                set.search(elNotInSet[j]);
             }
-            long endTime1=System.nanoTime();
-            long executionTime1= endTime1 - startTime1;
-            double totalTime1 = executionTime1/(double)exper;
+            long endTime1 = System.nanoTime();
+            timeNotIn[i] = ((endTime1 - startTime1) / (double) exper) / 1000.0;
 
             SparseSet newSet = generateRandomSet(maxVal, capacity, size);
             long startUnion = System.nanoTime();
-            for (int i = 0;i<exper;i++){
+            for (int j = 0;j<exper;j++){
                 set.union(newSet);
             }
             long endUnion = System.nanoTime();
             long executionUnion = endUnion - startUnion;
-            double totalUnion = executionUnion/(double)exper;
+            timeUnion[i] = ((endUnion - startUnion) / (double) exper) / 1000.0;
 
-            System.out.println("Size of the set: " + size + "\nTime to search for an element that is in a set: "+ totalTime + "\nTime to search for an element that is not in the set: "+ totalTime1 +"\nTime for uniom: "+ totalUnion);
+            System.out.println("Size of the set: " + size + "\nTime to search for an element that is in a set: "+ timeIn[i] + "\nTime to search for an element that is not in the set: "+ timeNotIn[i]  +"\nTime for uniom: "+ timeUnion[i]);
+
         }
-
-
+        new ResultsChart(sizes, timeIn, timeNotIn, timeUnion);
     }
 
     private static SparseSet generateRandomSet(int maxVal, int capacity, int size) {

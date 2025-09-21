9be241702f1764383ad0e0fb2bfcc14c578f2eba
Arina <a.zvarunchyk@gmail.com>
2025-09-21 16:37:18 +0300

    тестування орієнт графа

--- lab2/Test.java
+++ lab2/Test.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         testUndirected();
+        testDirected();
     }
 
     public static void printGraph(Graph<Integer> g) {
@@ -49,6 +50,34 @@ public class Test {
         int[][] matrix = g.convertToAdjMatrix();
         System.out.println("Adjacency Matrix: ");
         for (int[] row : matrix){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println();
+
+        g.BackAdjMatrix(matrix);
+        printGraph(g);
+    }
+
+    public static void testDirected() {
+        System.out.println("Testing Directed Graph");
+        Directed g = new Directed(3);
+        g.addEdge(0,1);
+        g.addEdge(1,2);
+        printGraph(g);
+
+        g.deleteEdge(0,1);
+        printGraph(g);
+
+        g.addVertex();
+        g.addEdge(2,3);
+        printGraph(g);
+
+        g.deleteVertex(1);
+        printGraph(g);
+
+        int[][] matrix = g.convertToAdjMatrix();
+        System.out.println("Adjacency Matrix: ");
+        for (int[] row : matrix) {
             System.out.println(Arrays.toString(row));
         }
         System.out.println();

f791072b1ed30bf3dc81bbc74f1b094d82717243
Arina <a.zvarunchyk@gmail.com>
2025-10-20 03:37:46 +0300

    edge

---
+++ lab4/Edge1.java
@@ -0,0 +1,24 @@
+public final class Edge1 implements Comparable<Edge1>{
+    private final int u;
+    private final int v;
+    private final long w;
+    public Edge1(int u,int v,long w){
+        this.u=u;
+        this.v=v;
+        this.w=w;
+    }
+    public int getU(){
+        return u;
+    }
+    public int getV(){
+        return v;
+    }
+    public long getW() {
+        return w;
+    }
+
+    @Override
+    public int compareTo(Edge1 other) {
+        return  Long.compare(this.w, other.w);
+    }
+}

93081d71f5c34afb8c21aef468743d6fa5a82079
Arina <a.zvarunchyk@gmail.com>
2025-10-20 03:14:16 +0300

    lab4

--- lab4/UnionFindList.java
+++ lab4/UnionFindList.java
@@ -1,9 +1,11 @@
 public class UnionFindList {
     private int n;
     private int[] head, tail, root, next, size;
+
     public UnionFindList (int n){
         makeSet(n);
     }
+
     public void makeSet(int n){
         if(n<0){
             throw new IllegalArgumentException("n must be >=0");
@@ -19,11 +21,69 @@ public class UnionFindList {
             head[i]=i;
             tail[i]=i;
             root[i]=i;
-            next[i]= -1;;
+            next[i]= -1;
             size[i]=1;
         }
     }
 
+    private void validateIndex(int x) {
+        if (x < 0 || x >= n) throw new IllegalArgumentException("index out of range: " + x);
+    }
 
-}
+    public int find(int x){
+        validateIndex(x);
+        return root[x];
+    }
+
+    public boolean sameSet(int x, int y) {
+        validateIndex(x);
+        validateIndex(y);
+        return find(x) == find(y);
+    }
+
+    public int sizeOf(int x) {
+        validateIndex(x);
+        return size[find(x)];
+    }
+
+    public void union(int x,int y){
+        validateIndex(x);
+        validateIndex(y);
+
+        int rx=find(x);
+        int ry=find(y);
+
+        if(rx==ry){
+            return;
+        }
+
+        if(size[rx]<size[ry]){
+            int temp = rx;
+            rx=ry;
+            ry=temp;
+        }
+        git add .
+
+        next[tail[rx]] = head[ry];
+
+        int cur = head[ry];
+        while (cur != -1) {
+            root[cur] = rx;
+            cur = next[cur];
+        }
+
+        tail[rx] = tail[ry];
+        size[rx] += size[ry];
+
+        head[ry] = -1;
+        tail[ry] = -1;
+        size[ry] = 0;
+    }
+
+    }
+
+
+
+
+
 
